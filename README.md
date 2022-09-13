5c49a435b7a4c414a37d076d826ff0f8f322b115
Lincoln Dias Marques <licodmarques@gmail.com>
2022-09-12 21:22:58 -0300

    Módulo 1 finalizado

---
+++ README.md
@@ -0,0 +1,7 @@
+# Projeto criado para a semana NLW eSports
+
+### Realizado no dia 11/09 até 18/09 em 2022
+
+- Tecnologias utilizada no Server:
+- Tecnologias utilizada na Web:
+- Tecnologias utilizada no Mobile:
---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Brunin viadim</Text>
+      <StatusBar style="auto" backgroundColor="#a7a7a7" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 44,
+  },
+});
